79ebcd4810991fc08afed2c7de473ab6fe1b61b8
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-15 17:14:22 -0300

    [App] - Homepage page created

--- movie-reviewer/src/App.js
+++ movie-reviewer/src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+import HomePage from './pages/homepage/homepage.component';
+
+// App.js converted to a Class Component
+class App extends React.Component {
+
+  componentDidMount(){
+    console.log("Testing homepage component")
+  }
+
+  render(){
+    return (
+      <div>
+        <Switch> {/* Switch matches only one and nothing more after it. Unique router */}
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ movie-reviewer/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <h1>JWow Homepage</h1>
+    </div>
+);
+
+export default HomePage;
