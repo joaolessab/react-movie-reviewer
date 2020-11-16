070514242d02692dc4bd43569db8197be8af2f43
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-15 22:01:04 -0300

    [Homepage] - Calling Movies Panel

--- movie-reviewer/src/pages/homepage/homepage.component.jsx
+++ movie-reviewer/src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import './homepage.styles.scss';
 
+import MoviesPanel from '../../components/movies-panel/movies-panel.component';
+
 const HomePage = () => (
     <div className='homepage'>
-        <h1>JWow Homepage</h1>
+        <h1>Movies Jwow likes!</h1>
+        <MoviesPanel />
     </div>
 );
 
