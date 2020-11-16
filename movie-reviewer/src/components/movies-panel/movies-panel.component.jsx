b538292751defd3e1b20bfb96ea3494ee2ea456a
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-11-15 23:26:17 -0300

    [Movie Reviewer] - Component body

--- movie-reviewer/src/components/movies-panel/movies-panel.component.jsx
+++ movie-reviewer/src/components/movies-panel/movies-panel.component.jsx
@@ -9,10 +9,11 @@ import './movies-panel.styles.scss';
 const MoviesPanel = ({ movies }) => (
     <div>
         { movies.map(movie => (
+            /* MovieItem: It could break into a component, but it didn't see what is it, right now */
             <div key={movie.id}>
                 <p>
-                    <span>{`${movie.score}%`}</span>&nbsp; 
-                    <span>{`${movie.title}`}</span>&nbsp;
+                    <span>{`${movie.score * 100}%`}</span>&nbsp;
+                    <span onClick={()=> window.open(`${movie.url}`, "_blank")}>{`${movie.title}`}</span>&nbsp;
                     <span>{`${movie.year}`}</span>
                 </p>
             </div>
